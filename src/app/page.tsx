b86faf25c0feabc7508d5fa96383f48fc11e7a2b
Giovanni <giovannicruzvinhedo@gmail.com>
2025-03-05 14:37:17 -0300

    feat: user session control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      home
-    </div>
-  );
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { redirect, RedirectType } from "next/navigation"
+
+export default async function Home() {
+  let loggedIn = false
+  try {
+    const supabase = createServerComponentClient({ cookies })
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
+
+    if (session) loggedIn = true
+  } catch (error) {
+    console.log("Home", error)
+  } finally {
+    if (loggedIn) redirect("/user-app", RedirectType.replace)
+  }
+
+  return <div className="text-lime-700">home</div>
 }
