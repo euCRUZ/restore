8a11195327bafc0717a34dcb788cbe822d2af1c9
Giovanni <giovannicruzvinhedo@gmail.com>
2025-03-12 17:16:03 -0300

    feat: CreateAccount form structure with zod done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import { redirect, RedirectType } from "next/navigation"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import CreateAccountForm from "@/components/auth/create-account-form"
 
 export default async function Home() {
   let loggedIn = false
@@ -31,56 +21,31 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col h-screen w-full justify-center items-center">
-      <Tabs defaultValue="create-account" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="create-account" className="cursor-pointer">Account</TabsTrigger>
-          <TabsTrigger value="login" className="cursor-pointer">Login</TabsTrigger>
+      <Tabs
+        defaultValue="create-account"
+        className="w-[400px] border rounded-md pb-4 shadow-2xl"
+      >
+        <TabsList className="w-full flex justify-around items-center rounded-b-none h-14">
+          <TabsTrigger
+            value="create-account"
+            className="cursor-pointer transition-all delay-150"
+          >
+            Account
+          </TabsTrigger>
+          <TabsTrigger
+            value="login"
+            className="cursor-pointer transition-all delay-100"
+          >
+            Login
+          </TabsTrigger>
         </TabsList>
+
         <TabsContent value="create-account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Create Your Account</CardTitle>
-              <CardDescription>
-                Make changes to your account here. Click save when you&apos;re done.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name"  placeholder="Your name"/>
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" placeholder="Your username" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
+          <CreateAccountForm />
         </TabsContent>
+
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you&apos;ll be logged out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
+          
         </TabsContent>
       </Tabs>
     </div>
---
+++ src/components/auth/create-account-form.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Must be a valid email",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, {
+      message: "Password must be at least 8 characters",
+    })
+    .max(16),
+})
+
+const CreateAccountForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center space-y-2">
+      <span className="text-lg">You&apos;ll love it.</span>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="E-mail" {...field} />
+                </FormControl>
+                <FormDescription>This is your E-mail.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormDescription>This is your Password.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            Create Account
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default CreateAccountForm
